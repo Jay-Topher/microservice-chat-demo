8c3d655d2bed657e636f08454f596a8d06b69525
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2021-09-16 09:30:18 +0100

    db connection

---
+++ users-service/src/db/connection.ts
@@ -0,0 +1,18 @@
+import config from "config";
+import { Connection, createConnection } from "typeorm";
+import User from "./entities/User";
+import UserSession from "./entities/UserSessions";
+
+let connection: Connection;
+
+export const initConnection = async () => {
+  connection = await createConnection({
+    entities: [User, UserSession],
+    type: "mysql",
+    url: <string>config.get("USERS_SERVICE_DB_URL"),
+  });
+};
+
+const getConnection = () => connection;
+
+export default getConnection;
